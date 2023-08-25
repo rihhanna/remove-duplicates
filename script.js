afeff25fc18d4c5e02d007688c2d14e53d3855e0
rihhana <hrihhana@gmail.com>
2023-08-25 18:27:34 +0300

    done it

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Good Luck 💪🏾
+function removeDuplicates(arr) {
+    // create an empty array to store the unique items
+    let unique = [];
+    // loop through the original array
+    for (let i = 0; i < arr.length; i++) {
+      // check if the current item is already in the unique array
+      if (!unique.includes(arr[i])) {
+        // if not, push it to the unique array
+        unique.push(arr[i]);
+      }
+    }
+    // return the unique array
+    return unique;
+  }
+  console.log(removeDuplicates([1, 0, 1, 0]))
+  console.log(removeDuplicates(["New", "York", "City"]))// ➞ ["New", "York", "City"]
+  console.log(removeDuplicates(["Hassan", "Aisha", "Aisha"])) //➞ ["Hassan", "Aisha"]
+  
